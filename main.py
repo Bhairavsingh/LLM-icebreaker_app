82ca4ce81a9a97411e811087156069110ae859ac
bhairavsinghghorpade <Bhairavsingh.Ghorpade@gmail.com>
2024-07-10 17:18:57 -0500

    LinkedIn Scraper

--- main.py
+++ main.py
@@ -6,27 +6,21 @@ import os
 
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
+from linkedin import linkedin_scraper
 
-
-information = """
-"""
 
 if __name__ == "__main__":
     load_dotenv()
-    print(os.environ['API_KEY'])
 
     summary_template = """
-        From the given information {information} about a person create following"
+        From the given LinkedIn information {information} about a person create following"
         1. A short summary.
         2. Two interesting facts about them.
     """
 
-    summary_prompt_tenplate = PromptTemplate(input_variables = ["information"], template = summary_template)
-
+    summary_prompt_template = PromptTemplate(input_variables = ["information"], template = summary_template)
     llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
-
-    chain = summary_prompt_tenplate | llm
-
-    res = chain.invoke(input = {"information" : information})
-
-    pass
+    chain = summary_prompt_template | llm
+    linkedin_information = linkedin_scraper(linkedin_profile_link = "https://www.linkedin.com/in/bhairavsingh-ghorpade/")
+    res = chain.invoke(input = {"information" : linkedin_information})
+    print(res)
