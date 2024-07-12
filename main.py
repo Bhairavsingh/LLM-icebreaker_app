0ad2deb74b20b56aa24c5accaa1fd7397df2428b
bhairavsinghghorpade <Bhairavsingh.Ghorpade@gmail.com>
2024-07-12 12:56:45 -0500

    Adding Output Parser

--- main.py
+++ main.py
@@ -8,25 +8,41 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from external_bots.linkedin import linkedin_scraper
 from agent.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parser import summary_parser
 
 
 # Function for
 def ice_breaker_with(name: str):
-    pass
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_information = linkedin_scraper(linkedin_profile_link=linkedin_url)
+
+    summary_template = """
+            From the given LinkedIn information {information} about a person create following"
+            1. A short summary.
+            2. Two interesting facts about them.
+            
+            \n{format_instructions} 
+        """
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"],
+                                             template=summary_template,
+                                             partial_variables={"format_instructions": summary_parser.get_format_instructions()},)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    chain = summary_prompt_template | llm | summary_parser
+    res = chain.invoke(input={"information": linkedin_information})
+
+    return res
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    summary_template = """
-        From the given LinkedIn information {information} about a person create following"
-        1. A short summary.
-        2. Two interesting facts about them.
-    """
+    print("Ice Breaker!")
 
-    summary_prompt_template = PromptTemplate(input_variables = ["information"], template = summary_template)
-    llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
-    chain = summary_prompt_template | llm
-    linkedin_information = linkedin_scraper(linkedin_profile_link = "https://www.linkedin.com/in/bhairavsingh-ghorpade/")
-    res = chain.invoke(input = {"information" : linkedin_information})
-    print(res)
+    result = ice_breaker_with(name='Nicholas Salter Space Cow')
+
+    print(result.summary)
+    print(result.facts)
+
+
+
